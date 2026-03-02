cca7c83b63d28a75ae94a5a04539aba20c32c7a4
SunkireddyBarath <sunkireddybarath07@gmail.com>
2026-03-02 20:49:57 +0530

    feature added

--- start-backend-dev.sh
+++ start-backend-dev.sh
@@ -1,7 +1,5 @@
-#!/bin/sh
-# Move to backend directory
 cd fhir-backend-dynamic || exit 1
-# Activate virtual environment
+
 . .venv/bin/activate
-# Run FastAPI
+
 uvicorn app.main:app --reload --host 0.0.0.0 --port 8000
